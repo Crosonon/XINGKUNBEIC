aab8febcaf21929ae30cbc508aab94e1ccf26d5a
daefghjk <2105049655@qq.com>
2025-04-22 23:16:28 +0800

    修改TIM3 Arr 1ms触发一次

--- stm32f103c8t6/Core/Src/tim.c
+++ stm32f103c8t6/Core/Src/tim.c
@@ -126,7 +126,7 @@ void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 72-1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 10000-1;
+  htim3.Init.Period = 1000-1;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -146,7 +146,6 @@ void MX_TIM3_Init(void)
   }
   /* USER CODE BEGIN TIM3_Init 2 */
 
-  htim3.Init.Period = 1000-1;//这个配置文件不知为何一直无法生成这一块的正式代码
   /* USER CODE END TIM3_Init 2 */
 
 }
